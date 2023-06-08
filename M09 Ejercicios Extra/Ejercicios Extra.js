ea1058c7042cba5227d5611965d81dc328d55b0b
FatimaSanchez <sanchezfatimaalexandra@gmail.com>
2023-06-07 22:56:33 -0300

    Resolución módulo 7,8 y 9

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,14 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var arrayPadre = [];
+   for (let prop in objeto) {
+      var arrayHijo = [];
+      arrayHijo.push(prop);
+      arrayHijo.push(objeto[prop]);
+      arrayPadre.push(arrayHijo);
+   }
+   return arrayPadre;
 }
 
 function numberOfCharacters(string) {
@@ -14,7 +22,23 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   var objeto = {};
+   var stringComoArreglo = string.split('');
+   var stringOrdenada = stringComoArreglo.sort();
+   let contador = 0;
+   for (let i=0; i<stringOrdenada.length; i++) {
+      if (stringOrdenada[i] === stringOrdenada[i+1]) {
+         contador += 1;
+      } else {
+         contador += 1;
+         objeto[stringOrdenada[i]] = contador;
+         contador = 0;
+      }
+   }
+   return objeto;
 }
+
+
 
 function capToFront(string) {
    // Recibes un string con algunas letras en mayúscula y otras en minúscula.
@@ -22,6 +46,15 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   var mayus = '';
+   var minus = '';
+   for (let i=0; i<string.length; i++) {
+      if (string[i] === string[i].toUpperCase()) {
+         mayus += string[i];
+      }
+      else minus += string[i];
+   }
+   return mayus+minus;
 }
 
 function asAmirror(frase) {
@@ -29,18 +62,37 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   var palabras = frase.split(' ');
+   var FraseInvertida = [];
+   for (var i=0; i<palabras.length; i++) {
+      var palabraInvertida = palabras[i].split('').reverse();
+      FraseInvertida.push(palabraInvertida.join(''));
+   }
+   return FraseInvertida.join(' ');
 }
+
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   if (numero.toString()=== numero.toString().split('').reverse().join('')) {
+      return 'Es capicua';
+   }
+   else return 'No es capicua';
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   var stringModificado = '';
+   for (let i=0; i<string.length; i++) {
+      if (string[i] !== 'a' && string[i] !== 'b' && string[i] !== 'c') {
+         stringModificado += string[i];
+      }
+   }
+   return stringModificado;
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +101,19 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   for(let i=0; i<arrayOfStrings.length;i++) {        //beautiful
+      let min = arrayOfStrings[i];   
+      let posmin = i;                 //min = beautiful
+      for(let j=i+1; j<arrayOfStrings.length;j++) {   // looking
+         if(arrayOfStrings[j].length<min.length) {
+            min = arrayOfStrings[j];    
+            posmin = j;              //min = looking
+         }
+      }
+      arrayOfStrings[posmin] = arrayOfStrings[i];
+      arrayOfStrings[i] = min;
+   }
+   return arrayOfStrings;
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +123,15 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   var newArray = [];
+   for (let i=0; i<array1.length; i++) {
+      for (let j=0; j<array2.length; j++) {
+         if (array1[i] === array2[j]) {
+            newArray.push(array1[i]);
+         }
+      }
+   }
+   return newArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
